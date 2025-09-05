3c73c87f39c804815c244257d485b30b85455397
Anjun Feng <u7277205@anu.edu.au>
2025-09-05 13:05:14 +1000

    chore: add tasks

---
+++ oop/dog.py
@@ -0,0 +1,42 @@
+class Dog:
+    species = "Canis familiaris"
+
+    # Constructor for the Dog class
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # String representation of the Dog object
+    # This is how we customise the print output for an object in its Class
+    # E.g., if we do print(miles), it should return "Miles is 5 years old"
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+# --------- Inheritance Examples --------- #
+class GoldenRetriever(Dog):
+    def speak(self, sound="Arf"):
+        return f"{self.name} says {sound}"
+
+class AmericanEskimo(Dog):
+    def speak(self, sound="Arf!"):
+        return super().speak(sound)
+
+class Bulldog(Dog):
+    def speak(self, sound="Woof!"):
+        return super().speak(sound)
+    
+bob = Dog("Bob", 4)
+miles = Bulldog("Miles", 5)
+kenny = GoldenRetriever("Kenny", 3)
+
+print(bob)
+print(bob.speak("Woof Woof"))
+
+print(miles)
+print(miles.speak())
+
+print(kenny)
+print(kenny.speak())
---
+++ oop/task2.py
@@ -0,0 +1,93 @@
+"""
+Task 2: Create a class named Car with attributes like make, model, year, colour, and kilometerage.
+Include methods such as speed(), accelerate(), brake(), repaint(), and service().
+Then, create two child classes named Haval and Toyota that inherit from the Car class.
+Override at least one method in each child class to demonstrate polymorphism."""
+
+class Car:
+    def __init__(self, make, model, year, colour, kilometerage):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.colour = colour
+        self.kilometerage = kilometerage
+        self.speed = 0  
+
+    def __str__(self):
+        return f"This {self.colour} {self.make} car has {self.kilometerage} kilometers."
+    
+    def motorType(self, motor_type):
+        return f"This car has a {motor_type} motor."
+
+    def speed(self, units):
+        return f"Current speed: {self.speed} {units}."
+    
+    def accelerate(self, increase):
+        self.speed += increase
+        return f"Accelerated to {self.speed} km/h."
+    
+    def brake(self, decrease):
+        self.speed = max(0, self.speed - decrease)
+        return f"Slowed down to {self.speed} km/h."
+    
+    def repaint(self, new_colour):
+        self.colour = new_colour
+        return f"The car has been repainted to {self.colour}."
+
+    def service(self, is_serviced):
+        if is_serviced:
+            return "The car is being serviced."
+        else:
+            return "The car does not need servicing."
+
+class Haval(Car):
+    """
+    A child class representing a Haval car.
+    Inherits from Car and overrides the speed() method.
+    """
+    def __init__(self, model, year, colour, kilometerage):
+        # We call the parent's __init__ method, hardcoding 'Haval' for the make.
+        super().__init__('Haval', model, year, colour, kilometerage)
+
+    def speed(self, description='fast'):
+        """
+        Overrides the parent's speed method with a default value for the argument
+        """
+        if description == 'fast':
+            return f"This {self.make} {self.model} is going really fast!"
+        else:
+            # If a different description is provided, we can call the parent method
+            # to get a standard speed reading.ß 
+            return super().speed(description)
+        
+    def offroad(self, terrain_type):
+        return f"The {self.make} {self.model} is driving offroad on {terrain_type} terrain."
+
+class Toyota(Car):
+    """
+    A child class representing a Toyota car.
+    Inherits from Car and overrides the speed() method.
+    """
+    def __init__(self, model, year, colour, kilometerage):
+        # We call the parent's __init__ method, hardcoding 'Toyota' for the make.
+        super().__init__('Toyota', model, year, colour, kilometerage)
+
+    def speed(self, description='steady'):
+        """
+        Overrides the parent's speed method with a default value for the argument
+        """
+        if description == 'steady':
+            return f"This {self.make} {self.model} is cruising at a steady pace."
+        else:
+            # If a different description is provided, we can call the parent method
+            # to get a standard speed reading.ß 
+            return super().speed(description)
+        
+    def motorType(self, motor_type):
+        return super().motorType(motor_type) + " It is known for its reliability."
+
+
+''' *--------- An Example of Transitive Inheritance --------- # '''
+class Corolla(Toyota):
+    def __init__(self, year, colour, kilometerage):
+        super().__init__('Corolla', year, colour, kilometerage)
